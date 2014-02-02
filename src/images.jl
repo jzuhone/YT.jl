35a3fc3aed8a9a640362dec6a2dee1f777f4e619
John ZuHone <jzuhone@gmail.com>
2014-02-02 01:49:32 -0500

    Better handling of FixedResolutionBuffer

--- src/images.jl
+++ src/images.jl
@@ -9,6 +9,17 @@ import Base.show
 
 type FixedResolutionBuffer
     frb::PyObject
+    limits::Dict
+    buff_size::Tuple
+    function FixedResolutionBuffer(frb::PyObject)
+        limits = frb[:limits]
+        for k in keys(limits)
+            if limits[k] != nothing
+                limits[k] = YTArray([limits[k][1][1],limits[k][end][1]], "code_length")
+            end
+        end
+        new(frb, limits, frb[:buff_size])
+    end
 end
 
 function getindex(frb::FixedResolutionBuffer, key::String)
@@ -16,7 +27,14 @@ function getindex(frb::FixedResolutionBuffer, key::String)
 end
 
 function show(io::IO, frb::FixedResolutionBuffer)
-    println(io,frb.frb[:__repr__]())
+    println(io,"FixedResolutionBuffer ($(frb.buff_size[1])x$(frb.buff_size[2])):")
+    for k in keys(frb.limits)
+        if frb.limits[k] != nothing
+            a = frb.limits[k][1]
+            b = frb.limits[k][2]
+            println(io,"    $(a.value) $(a.units) <= $k < $(b.value) $(b.units)")
+        end
+    end
 end
 
 end

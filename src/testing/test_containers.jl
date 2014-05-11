1575d36309c6f694bacac3501bef92d3c9086b88
John ZuHone <jzuhone@gmail.com>
2014-05-11 01:42:25 -0400

    Some more tests

--- src/testing/test_containers.jl
+++ src/testing/test_containers.jl
@@ -1,8 +1,27 @@
-using Base.Test
-using jt
-using PyCall
-
-function run_container_tests(cont::DataContainer)
-
+function run_container_tests(dc::DataContainer)
+    max_jt = maximum(dc["density"]).value
+    min_jt = minimum(dc["density"]).value
+    sum_jt = sum(dc["density"]).value
+    len_jt = length(dc["density"])
+    min_yt, max_yt = dc.cont[:quantities][:extrema][:__call__]("density")
+    sum_yt = dc.cont[:quantities][:total_quantity][:__call__]("density")
+    len_yt = int(dc.cont[:quantities][:total_quantity][:__call__]("ones"))[1]
+    @test_approx_eq max_jt max_yt
+    @test_approx_eq min_jt min_yt
+    @test_approx_eq sum_jt sum_yt
+    @test len_jt == len_yt
 end
 
+ds = load("/Users/jzuhone/yt/test_outputs/GasSloshing/sloshing_nomag2_hdf5_plt_cnt_0100")
+
+dd = AllData(ds)
+
+run_container_tests(dd)
+
+sp1 = Sphere(ds, "c", (100.,"kpc"))
+sp2 = Sphere(ds, "max", (3.0856e22,"cm"))
+sp3 = Sphere(ds, [0.0,0.0,0.0], (0.2,"unitary"))
+
+run_container_tests(sp1)
+run_container_tests(sp2)
+run_container_tests(sp3)
---
+++ src/testing/tests.jl
@@ -0,0 +1,5 @@
+using Base.Test
+using jt
+
+include("test_containers.jl")
+include("test_array.jl")

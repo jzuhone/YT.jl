4958b7b05d402e56d36cf58d75e2d8c3fedca473
John ZuHone <jzuhone@gmail.com>
2014-05-10 23:39:10 -0400

    Getting rid of profiles

--- src/jt.jl
+++ src/jt.jl
@@ -16,18 +16,13 @@ export load, load_uniform_grid, load_amr_grids, load_particles
 
 # DataContainers
 
-export DataContainer, CutRegion, Disk, Ray, Boolean, Slice
+export DataContainer, CutRegion, Disk, Ray, Slice
 export Sphere, AllData, Projection, CoveringGrid, Grids, CuttingPlane
 export cut_region
 
 # Fixed resolution
 
 export FixedResolutionBuffer, to_frb
-
-# Profiles
-
-export Profile1D, Profile2D, Profile3D, add_fields
-export set_field_unit, set_x_unit, set_y_unit, set_z_unit
 
 # Plotting
 
@@ -48,12 +43,10 @@ include("profiles.jl")
 
 import .yt_array: YTArray, YTQuantity, in_units, in_cgs
 import .data_objects: Dataset, Grids, Sphere, AllData, Projection, Slice,
-    CoveringGrid, to_frb, print_stats, get_smallest_dx, Disk, Ray, Boolean,
+    CoveringGrid, to_frb, print_stats, get_smallest_dx, Disk, Ray,
     CuttingPlane, CutRegion, cut_region, DataContainer
 import .plots: SlicePlot, ProjectionPlot, PhasePlot, ProfilePlot, show_plot
 import .images: FixedResolutionBuffer
-import .profiles: Profile1D, Profile2D, Profile3D, add_fields, set_x_unit,
-    set_y_unit, set_z_unit, set_field_unit
 
 load(fn::String; args...) = Dataset(yt.load(fn; args...))
 

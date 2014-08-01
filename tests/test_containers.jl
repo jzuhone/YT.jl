450b4919453fa567b99ffd2f74a772fa3e5f09a2
John ZuHone <jzuhone@gmail.com>
2014-08-01 01:35:04 -0400

    Refactor of container tests, which are now direct tests against yt versions

--- tests/test_containers.jl
+++ tests/test_containers.jl
@@ -1,18 +1,18 @@
 using Base.Test
 using jt
+using PyCall
 
-function run_container_tests(dc::DataContainer)
-    max_jt = maximum(dc["density"]).value
-    min_jt = minimum(dc["density"]).value
-    sum_jt = sum(dc["density"]).value
-    len_jt = length(dc["density"])
-    min_yt, max_yt = dc.cont[:quantities][:extrema][:__call__]("density")
-    sum_yt = dc.cont[:quantities][:total_quantity][:__call__]("density")
-    len_yt = int(dc.cont[:quantities][:total_quantity][:__call__]("ones"))[1]
-    @test_approx_eq max_jt max_yt
-    @test_approx_eq min_jt min_yt
-    @test_approx_eq sum_jt sum_yt
-    @test len_jt == len_yt
+@pyimport yt.funcs as yt_funcs
+
+function check_container(dc::DataContainer; args=[], kwargs=[])
+    cont_name = yt_funcs.camelcase_to_underscore(repr(typeof(dc)))
+    py_dc = pycall(dc.ds.ds[cont_name], PyObject, args...)
+    for field in ["density", "temperature", "velocity_magnitude"]
+        a = dc[field]
+        b = pycall(py_dc["__getitem__"], PyObject, field)
+        @test all(a.value .== PyArray(b))
+        @test a.units.unit_symbol == b[:units]
+    end
 end
 
 ds = load("GasSloshing/sloshing_nomag2_hdf5_plt_cnt_0100")
@@ -21,42 +21,53 @@ ds = load("GasSloshing/sloshing_nomag2_hdf5_plt_cnt_0100")
 
 dd = AllData(ds)
 
-run_container_tests(dd)
+check_container(dd)
 
 # Spheres
 
-sp1 = Sphere(ds, "c", (100.,"kpc"))
-sp2 = Sphere(ds, "max", (3.0856e22,"cm"))
-sp3 = Sphere(ds, [3.0856e22,-1.0e23,0], (0.2,"unitary"))
+args1 = "c", (100.,"kpc")
+args2 = "max", (3.0856e22,"cm")
+args3 = [3.0856e22,-1.0e23,0], (0.2,"unitary")
 
-run_container_tests(sp1)
-run_container_tests(sp2)
-run_container_tests(sp3)
+sp1 = Sphere(ds, args1...)
+sp2 = Sphere(ds, args2...)
+sp3 = Sphere(ds, args3...)
+
+check_container(sp1, args=args1)
+check_container(sp2, args=args2)
+check_container(sp3, args=args3)
 
 # Regions
 
-reg1 = Region(ds, "c", [-3.0856e23,-3.0856e23,-3.0856e23],
-              [3.0856e23,3.0856e23,3.0856e23])
-reg2 = Region(ds, "max", [-3.0856e23,-3.0856e24,-6.1712e23],
-              [6.1712e23, 3.0856e23, 3.0856e24])
+args1 = "c", [-3.0856e23,-3.0856e23,-3.0856e23], [3.0856e23,3.0856e23,3.0856e23]
+args2 = "max", [-3.0856e23,-3.0856e24,-6.1712e23], [6.1712e23, 3.0856e23, 3.0856e24]
+reg1 = Region(ds, args1...)
+reg2 = Region(ds, args2...)
 
-run_container_tests(reg1)
-run_container_tests(reg2)
+check_container(reg1, args=args1)
+check_container(reg2, args=args2)
 
 # Disks
 
-dk1 = Disk(ds, "c", [1.0,0.5,0.2], 3.0856e23, 3.0856e23)
-dk2 = Disk(ds, [-1.0,0.7,-0.3], [0.0,3.0856e22,3.0856e23], 4e22, 5e23)
+args1 = "c", [1.0,0.5,0.2], 3.0856e23, 3.0856e23
+args2 = [-1.0,0.7,-0.3], [0.0,3.0856e22,3.0856e23], 4e22, 5e23
+dk1 = Disk(ds, args1...)
+dk2 = Disk(ds, args2...)
 
-run_container_tests(dk1)
-run_container_tests(dk2)
+check_container(dk1, args=args1)
+check_container(dk2, args=args2)
 
 # Rays
 
 # Slices
 
-slc1 = Slice(ds, "z", 4e23)
-slc2 = Slice(ds, 1, 0.0)
+args1 = "z", 4e23
+args2 = 1, 0.0
+slc1 = Slice(ds, args1...)
+slc2 = Slice(ds, args2...)
+
+check_container(slc1, args=args1)
+check_container(slc2, args=args2)
 
 # Projections
 
@@ -66,8 +77,16 @@ prj3 = Projection(ds, "density", 1, data_source=sp1)
 
 # Cutting Planes
 
+args1 = [1.0, 0.5, 0.3], "c"
+args2 = [0.2, -0.3, -0.4], [3.0856e22, 3.0856e23, -1.0e23]
+cp1 = Cutting(ds, args1...)
+cp2 = Cutting(ds, args2...)
+
+check_container(cp1, args=args1)
+check_container(cp2, args=args2)
+
 # Cut Regions
 
-# Grids
-
 # Covering Grids
+
+# Grids

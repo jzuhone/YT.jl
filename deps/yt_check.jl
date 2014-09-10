8338745f585134d61a91c5229519c4135f0ff056
John ZuHone <jzuhone@gmail.com>
2014-09-10 09:44:43 -0400

    Simplifying the error message

--- deps/yt_check.jl
+++ deps/yt_check.jl
@@ -17,9 +17,7 @@ function check_for_yt()
     @pyimport yt
     yt_version = convert(VersionNumber, yt.__version__)
     if yt_version < min_version
-        err_msg = ("Your version of yt is not up to date. " *
-                   "You need at least version $min_version, " *
-                   "but your current version is $yt_version. ")
+        err_msg = "Your yt installation (v. $yt_version) is not up to date. "
         error(err_msg * inst_msg)
     end
     return yt_version

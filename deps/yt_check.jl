2a6cf108e3c9371628a6ab3b029758b262a5f986
John ZuHone <jzuhone@gmail.com>
2014-09-10 09:42:21 -0400

    Adding an informative message about where to find yt

--- deps/yt_check.jl
+++ deps/yt_check.jl
@@ -2,11 +2,16 @@ import PyCall: @pyimport
 
 min_version = v"3.0"
 
+inst_msg = ("Please install yt version $min_version or higher " *
+            "using Anaconda, pip, or the yt installation script " *
+            "found at http://yt-project.org.")
+
 function check_for_yt()
     try
         @pyimport yt
     catch
-        error("Cannot import yt! Is it installed in the current Python?")
+        err_msg = "Cannot import yt! "
+        error(err_msg * inst_msg)
     end
 
     @pyimport yt
@@ -14,8 +19,8 @@ function check_for_yt()
     if yt_version < min_version
         err_msg = ("Your version of yt is not up to date. " *
                    "You need at least version $min_version, " *
-                   "but your current version is $yt_version.")
-        error(err_msg)
+                   "but your current version is $yt_version. ")
+        error(err_msg * inst_msg)
     end
     return yt_version
 end

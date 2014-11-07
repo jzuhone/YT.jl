8296514198a68222a99ea27be20cacb3a5d6add6
John ZuHone <jzuhone@gmail.com>
2014-11-07 12:16:20 -0500

    New frb tests

--- test/test_frbs.jl
+++ test/test_frbs.jl
@@ -2,26 +2,22 @@ using Base.Test
 using YT
 using PyCall
 
+file_to_read = "test/frbs.h5"
+
 ds = load("enzo_tiny_cosmology/DD0046/DD0046")
 
 slc = Slice(ds, "z", 0.0)
 prj = Proj(ds, "density", "z")
 
-py_slc = ds.ds[:slice]("z", 0.0)
-py_prj = ds.ds[:proj]("density", "z")
-
 frb1 = to_frb(slc, (500.,"kpc"), 800)
 frb2 = to_frb(prj, (500.,"kpc"), 800)
 
-pyfrb1 = pycall(py_slc["to_frb"], PyObject, (500.,"kpc"), 800)
-pyfrb2 = pycall(py_prj["to_frb"], PyObject, (500.,"kpc"), 800)
-
 a = frb1["density"]
-b = pycall(pyfrb1["__getitem__"], PyObject, "density")
-@test all(a.value .== PyArray(b))
-@test a.units.unit_symbol == b[:units]
+b = from_hdf5(file_to_read, dataset_name="frb1")
+@test all(a.value .== b.value)
+@test a.units == b.units
 
 a = frb2["density"]
-b = pycall(pyfrb2["__getitem__"], PyObject, "density")
-@test all(a.value .== PyArray(b))
-@test a.units.unit_symbol == b[:units]
+b = from_hdf5(file_to_read, dataset_name="frb2")
+@test all(a.value .== b.value)
+@test a.units == b.units

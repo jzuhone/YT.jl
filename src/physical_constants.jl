eb91c5e31eab33a2b983a91fc90ba33f8afdb462
John ZuHone <jzuhone@gmail.com>
2014-11-12 14:36:27 -0500

    New constants

--- src/physical_constants.jl
+++ src/physical_constants.jl
@@ -17,6 +17,16 @@ sigma_thompson = YTQuantity(ytutils.physical_constants["sigma_thompson"])
 amu_cgs = YTQuantity(ytutils.physical_constants["amu_cgs"])
 Na = 1 / amu_cgs
 Tcmb = YTQuantity(ytutils.physical_constants["Tcmb"])
-stefan_boltzmann = YTQuantity(ytutils.physical_constants["stefan_boltzmann_constant_cgs"])
+stefan_boltzmann_constant_cgs = YTQuantity(ytutils.physical_constants["stefan_boltzmann_constant_cgs"])
+hbar = 0.5*hcgs/pi
+m_pl = YTQuantity(ytutils.physical_constants["m_pl"])
+l_pl = YTQuantity(ytutils.physical_constants["l_pl"])
+t_pl = YTQuantity(ytutils.physical_constants["t_pl"])
+E_pl = YTQuantity(ytutils.physical_constants["E_pl"])
+T_pl = YTQuantity(ytutils.physical_constants["T_pl"])
+q_pl = YTQuantity(ytutils.physical_constants["q_pl"])
+mu_0 = YTQuantity(ytutils.physical_constants["mu_0"])
+eps_0 = YTQuantity(ytutils.physical_constants["eps_0"])
+k_e = 1.0/(4*pi*eps_0)
 
 end

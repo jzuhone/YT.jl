4fdd2590f2b450bef0f8e7c81954045b7cbd3678
John ZuHone <jzuhone@gmail.com>
2014-11-19 17:52:11 -0500

    This should fix the tests

--- test/test_containers.py
+++ test/test_containers.py
@@ -1,19 +1,20 @@
 import yt
 from yt.funcs import camelcase_to_underscore
+import numpy as np
 
 cont_dict = {}
 cont_dict["dd"] = "AllData", (), {}
-cont_dict["pt"] = "Point", ([0.5, 0.6, 0.2],), {}
+cont_dict["pt"] = "Point", (np.array([0.5, 0.6, 0.2]),), {}
 cont_dict["sp1"] = "Sphere", ("c", (500.,"kpc")), {}
-cont_dict["sp2"] = "Sphere", ([0.5,0.3,0.4], (0.2,"unitary")), {}
-cont_dict["reg"] = "Region", ("c", [0.1,0.1,0.1], [0.8,0.8,0.8]), {}
-cont_dict["dk1"] = "Disk", ("c", [1.0,0.5,0.2], 0.2, 0.2), {}
-cont_dict["dk2"] = "Disk", ([0.5,0.57,0.43], [-1.0,0.7,-0.3], 0.1, 0.3), {}
-cont_dict["ray"] = "Ray", ([0.1,0.1,0.4], [0.3,0.4,0.5]), {}
+cont_dict["sp2"] = "Sphere", (np.array([0.5,0.3,0.4]), (0.2,"unitary")), {}
+cont_dict["reg"] = "Region", ("c", np.array([0.1,0.1,0.1]), np.array([0.8,0.8,0.8])), {}
+cont_dict["dk1"] = "Disk", ("c", np.array([1.0,0.5,0.2]), 0.2, 0.2), {}
+cont_dict["dk2"] = "Disk", (np.array([0.5,0.57,0.43]), np.array([-1.0,0.7,-0.3]), 0.1, 0.3), {}
+cont_dict["ray"] = "Ray", (np.array([0.1,0.1,0.4]), np.array([0.3,0.4,0.5])), {}
 cont_dict["slc"] = "Slice", (1, 0.55), {}
-cont_dict["cp1"] = "Cutting", ([0.4, 0.5, 0.3], "c"), {}
-cont_dict["cp2"] = "Cutting", ([0.2, -0.3, -0.4], [0.3, 0.5, 0.4]), {}
-cont_dict["cvg"] = "CoveringGrid", (4, [0.2, 0.2, 0.2], [100,100,100]), {}
+cont_dict["cp1"] = "Cutting", (np.array([0.4, 0.5, 0.3]), "c"), {}
+cont_dict["cp2"] = "Cutting", (np.array([0.2, -0.3, -0.4]), np.array([0.3, 0.5, 0.4])), {}
+cont_dict["cvg"] = "CoveringGrid", (4, np.array([0.2, 0.2, 0.2]), np.array([100,100,100], dtype="int")), {}
 cont_dict["prj1"] = "Proj", ("density", "z"), {}
 cont_dict["prj2"] = "Proj", ("density", 2), {"weight_field":"temperature"}
 

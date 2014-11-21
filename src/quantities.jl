9374417a345731521e312e3fa9581e4666ac7fe7
John ZuHone <jzuhone@gmail.com>
2014-11-21 13:51:32 -0500

    More simplifying of derived_quantities

--- src/quantities.jl
+++ src/quantities.jl
@@ -1,6 +1,6 @@
 module quantities
 
-import PyCall: PyObject, pycall, pybuiltin, PyAny, @pyimport, pyisinstance
+import PyCall: PyObject, pycall, pybuiltin, PyAny, @pyimport, pyisinstance, PyVector
 import ..array: array_or_quan
 import ..data_objects: DataContainer
 
@@ -16,9 +16,8 @@ function derived_quantity(dc::DataContainer, key::String, args...)
     q = get(dc.cont["quantities"], key)
     a = pycall(q["__call__"], PyObject, args...)
     if pyisinstance(a, PyTuple) || pyisinstance(a, PyList)
-        n = a[:__len__]()
-        return [array_or_quan(get(a,i))
-                for i in 0:n-1]
+        a = PyVector{PyObject}(a)
+        return tuple([array_or_quan(v) for v in a]...)
     else
         return array_or_quan(a)
     end
@@ -28,8 +27,5 @@ for key in keys(dq.derived_quantity_registry)
     quan = symbol(ytfuncs.camelcase_to_underscore(key))
     @eval ($quan)(dc::DataContainer, args...) = derived_quantity(dc, $key, args...)
 end
-
-names = [symbol(ytfuncs.camelcase_to_underscore(key))
-         for key in keys(dq.derived_quantity_registry)]
 
 end

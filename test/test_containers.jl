87749ca6b00604021ded339392f5df884c665e0b
John ZuHone <jzuhone@gmail.com>
2015-01-30 12:41:20 -0500

    More data container tests

--- test/test_containers.jl
+++ test/test_containers.jl
@@ -87,6 +87,11 @@ vmax, cmax = YT.find_max(ds, "density")
 
 @test get_smallest_dx(ds) == minimum(dd["dx"])
 
+# Data source check
+
+dd_sp = YT.AllData(ds, data_source=sp2)
+@test sum(dd_sp["density"]) == sum(sp2["density"])
+
 # Quick field name check
 
 @test sp2["density"].value == sp2["gas","density"].value
@@ -100,3 +105,11 @@ YT.set_field_parameter(sp2, "center", [0.1,-0.3,0.2])
 fp_keys = collect(keys(YT.get_field_parameters(sp2)))
 
 @test sort(fp_keys) == ["bulk_velocity","center","normal","radius"]
+
+fps = ["num_dinosaurs"=>10000,"distance_from_earth"=>3000.]
+
+sp3 = Sphere(ds, cont_dict["sp2"][2]..., field_parameters=fps)
+@test YT.has_field_parameter(sp3, "num_dinosaurs")
+@test YT.has_field_parameter(sp3, "distance_from_earth")
+@test YT.get_field_parameter(sp3, "num_dinosaurs") == 10000
+@test YT.get_field_parameter(sp3, "distance_from_earth") == 3000.

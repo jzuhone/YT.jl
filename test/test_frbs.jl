40a22d791701313625ae88690045a17895679b9b
John ZuHone <jzuhone@gmail.com>
2015-01-30 12:35:18 -0500

    Small test

--- test/test_frbs.jl
+++ test/test_frbs.jl
@@ -14,6 +14,7 @@ cut = Cutting(ds, [0.4, 0.5, 0.3], "c")
 frb1 = to_frb(slc, YTQuantity(500.,"kpc"), 800)
 frb2 = to_frb(prj, (500.,"kpc"), 800)
 frb3 = to_frb(cut, (500.,"kpc"), 800)
+frb4 = to_frb(cut, YTQuantity(500.,"kpc"), 800)
 
 a = frb1["density"]
 b = from_hdf5(file_to_read, dataset_name="frb1")
@@ -29,6 +30,10 @@ a = frb3["density"]
 b = from_hdf5(file_to_read, dataset_name="frb3")
 @test all(a.value .== b.value)
 @test a.units == b.units
+
+c = frb4["density"]
+@test all(a.value .== c.value)
+@test a.units == c.units
 
 show(STDOUT, frb3)
 

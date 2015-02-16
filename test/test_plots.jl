f79e139b29da89804fc7ae022bcfb1c3f5517a38
John ZuHone <jzuhone@gmail.com>
2015-02-16 12:05:55 -0500

    Testing a couple more plot options

--- test/test_plots.jl
+++ test/test_plots.jl
@@ -6,7 +6,14 @@ using PyCall
 
 ds = load("enzo_tiny_cosmology/DD0046/DD0046")
 
+val, ctr = find_max(ds, "density")
+
 slc = SlicePlot(ds, "z", ["density","temperature"])
+slc.set_width(0.3,"unitary")
+slc.annotate_grids()
+slc.save()
+
+slc = SlicePlot(ds, "z", ["density","temperature"], center=ctr)
 slc.set_width(0.3,"unitary")
 slc.annotate_grids()
 slc.save()
@@ -16,8 +23,7 @@ prj.set_log("temperature", false)
 prj.save()
 
 rad = YTQuantity(0.5,"Mpc")
-val, ctr = find_max(ds, "density")
 sp = Sphere(ds, ctr, rad)
-prj = ProjectionPlot(ds, "z", ["temperature"], weight_field="density", data_source=sp)
+prj = ProjectionPlot(ds, "z", ["temperature"], weight_field="density", center=ctr, data_source=sp)
 prj.set_log("temperature", false)
 prj.save()
